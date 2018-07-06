c4336baec8f017bdf6916f9c906da6e571fdd5e7
Marcos Pereira Luiz <marcosplz@gmail.com>
2018-07-06 19:42:26 -0300

    Update VariavelConexaoUtil.java

--- aulasjsfprimefaces/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
+++ aulasjsfprimefaces/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -6,7 +6,8 @@ package br.com.framework.implementacao.crud;
  *
  */
 public class VariavelConexaoUtil {
-
+	
+	//Variável de conexão
 	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
 
 }
